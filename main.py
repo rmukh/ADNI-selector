2cfd513f7be7a8a60e4244c3809963ac4e6c4cd4
Rinat Mukhometzianov <mukhometzyanov@mail.ru>
2020-02-05 17:19:10 -0500

    exluded converges selector added + bugs fixed

--- main.py
+++ main.py
@@ -14,13 +14,23 @@ class DATA_PROCESS():
         # Check if ADNIMERGE.csv file is available
         return exists('ADNIMERGE.csv')
 
-    def selectNgenerate(self, stages, diags, age, age_range):
+    def selectNgenerate(self, stages, diags, age, age_range, include_convrgd):
         adni_full = pd.read_csv('ADNIMERGE.csv', skipinitialspace=True, usecols=self.fields)
 
         if len(diags) == 1:
-            self.filtered_groups = adni_full[adni_full.COLPROT.isin(stages) & adni_full.DX_bl.isin(diags)] # for emci and lmci group selection
+            self.filtered_groups = adni_full[adni_full.COLPROT.isin(stages) & adni_full.DX_bl.isin(diags)] # for classification group selection
+            
             if age != 0.0:
                 self.filtered_groups = self.filtered_groups[self.filtered_groups.AGE.between(age - age_range, age + age_range)]
+            
+            self.filtered_groups = self.filtered_groups.sort_values(by=['Month'])
+
+            if not include_convrgd:
+                self.filtered_groups = self.filtered_groups.dropna(subset=['DX'])
+                
+                self.groups = self.filtered_groups.groupby('RID', sort=False)
+                self.filtered_groups = self.groups.filter(lambda x: x.DX.eq(diags[0]).all())
+
         else:
             self.phase = adni_full[adni_full.COLPROT.isin(stages) & adni_full.DX.isin(diags)]
 
@@ -30,7 +40,8 @@ class DATA_PROCESS():
             self.phase = self.phase.sort_values(by=['Month'])
     
             self.groups = self.phase.groupby('RID', sort=False)
-    
+            
+            # Filter subjects that are converged from diags[0] to diags[1]
             self.filtered_groups = self.groups.filter(lambda x: x.DX.eq(diags[0]).values[0] & x.DX.eq(diags[1]).values[-1])
         
         self.u_rids = self.filtered_groups.RID.unique()
@@ -78,13 +89,15 @@ class GUI():
                      sg.Radio('MCI', "g", size=(8,1), font=("Open Sans", 11)), 
                      sg.Radio('AD', "g", size=(8,1), font=("Open Sans", 11))],
                     [sg.Radio('EMCI', "g", size=(8,1), font=("Open Sans", 11)), 
-                     sg.Radio('LMCI', "g", size=(8,1), font=("Open Sans", 11))]
+                     sg.Radio('LMCI', "g", size=(8,1), font=("Open Sans", 11)),
+                     sg.Checkbox('stable only?', font=("Open Sans", 11), default=True, 
+                                 tooltip='Include subjects that have been converged to other groups?')]
                     ], title='Classification groups', relief=sg.RELIEF_SUNKEN, font=("Open Sans", 14))
             ],
             
             [sg.Text('Age', font=("Open Sans", 11)), 
              sg.InputText(size=(5,1), default_text='0.0', font=("Open Sans", 10)),
-             sg.Text('±', font=("Verdana", 12)),
+             sg.Text(u'\xb1', font=("Default 12", 12)), # plus minus symbol
              sg.InputText(size=(5,1), default_text='0.0', font=("Open Sans", 10)),
              sg.Checkbox('Save to file', font=("Open Sans", 11))],
             [sg.Text('RIDs', font=("Open Sans", 16))],
@@ -121,9 +134,9 @@ class GUI():
     def age_check(self):
         age = 0.0
         age_range = 0.0
-        if self.values[15].replace('.','',1).isdigit() and self.values[16].replace('.','',1).isdigit():
-            age = float(self.values[15])
-            age_range = float(self.values[16])
+        if self.values[16].replace('.','',1).isdigit() and self.values[17].replace('.','',1).isdigit():
+            age = float(self.values[16])
+            age_range = float(self.values[17])
             if age < 0.0 or age_range < 0.0:
                 sg.Popup('Age can\'t be negative. Will be ignored in the final output.')
                 age = 0.0
@@ -146,7 +159,7 @@ class GUI():
                     self.stages = [self.menu_map_stage[i] for i in range(4) if self.values[i]]
                     self.groups = [self.menu_map_group[i] for i in range(4, 12) if self.values[i]][0]
 
-                    res = self.dp.selectNgenerate(self.stages, self.groups, age, age_range)
+                    res = self.dp.selectNgenerate(self.stages, self.groups, age, age_range, self.values[15])
                     self.res_rid = ', '.join([str(i) for i in res])
 
                     if self.res_rid == '': # if no results with specified input params
@@ -161,7 +174,7 @@ class GUI():
                         self.main_window['-res-rid_dates-'].update(self.res_rid_dates)
 
                         # Write to file if selected and there are results to write
-                        if self.values[17]:
+                        if self.values[18]:
                             self.write_to_file()
                 else:
                     sg.Popup('ADNIMERGE.csv not found!', 'Please, put it in the same folder.')
